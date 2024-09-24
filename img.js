56e144b4d60257d28ae2e2473c10ab264149408c
Harsh <harshgupt100@gmail.com>
2024-09-24 12:28:14 +0530

    first commit

---
+++ img.js
@@ -0,0 +1,4 @@
+function change(){
+    let selecteddiv = document.querySelector('#img1');
+    selecteddiv.style.backgroundImage = "url('https://images.unsplash.com/photo-1726942371143-3afca583a72f?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8')";
+}
